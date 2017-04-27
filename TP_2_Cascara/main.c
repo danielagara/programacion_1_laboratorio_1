7300f92cad9c94587aca10d719b6ad2798d046f4
danielagara <daniela_mariel_garat@hotmail.com>
2017-04-27 17:49:26 -0300

    TERMINADO FALTA CORRECCION DE GRAFICO

---
+++ TP_2_Cascara/main.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "funciones.h"
+#include "string.h"
+#define S 20
+
+typedef struct
+{
+    char nombre[20];
+    int edad;
+    int dni;
+    int estado;
+    char alta;
+} eDatosPersonales;
+
+
+void mostrarPersona(eDatosPersonales personas[], int cant);
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+    eDatosPersonales personas[S];
+    int i, j, u, k;
+    char respuesta;
+    int contador=0;
+    int contadorMenores=0, contadorEntre=0, contadorMayores=0;
+    int men, ent, may;
+
+    char columnaMenores[contadorMenores], columnaEntre[contadorEntre],columnaMayores[contadorMayores];
+    int auxDniBusqueda;
+    char flagBusqueda='n';
+    int longMenores, longEntre, longMayores;
+
+
+    for(i=0;i<S;i++)
+    {
+        personas[i].estado=-1;
+        personas[i].alta='n';
+
+    }
+
+    while(seguir=='s')
+    {
+        printf("\n\n1- Agregar persona\n"); //OK
+        printf("2- Borrar persona\n");//OK
+        printf("3- Imprimir lista ordenada por  nombre\n");//OK
+        printf("4- Imprimir grafico de edades\n\n");
+        printf("5- Salir\n");
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                while(contador<20)
+                {
+                    respuesta='s';
+                    while(respuesta=='s')
+                    {
+                    if(personas[contador].estado!=-1)
+                    {
+                        printf("NO HAY LUGAR");
+                        break;
+                    }
+                    else
+                    {
+                        personas[contador].estado=0;
+                        personas[contador].alta='s';
+                        for(j=contador+1;j<S;j++)
+                        {
+                            printf("Ingrese dni: \n\n");
+                            fflush(stdin);
+                            scanf("%d", &personas[j].dni);
+                            if(personas[contador].dni!=personas[j].dni)
+                            {
+                                printf("Ingrese el nombre: \n\n");
+                                fflush(stdin);
+                                gets(personas[j].nombre);
+                                personas[j].nombre[0]= toupper(personas[j].nombre[0]);
+
+                                printf("Ingrese la edad: \n\n");
+                                fflush(stdin);
+                                scanf("%d", &personas[j].edad);
+                                while(personas[j].edad<=0 || personas[j].edad>=100)
+                                {
+                                    printf("ERROR, Ingrese la edad: \n\n");
+                                    fflush(stdin);
+                                    scanf("%d", &personas[j].edad);
+                                }
+                                if(personas[j].edad<18)
+                                {
+                                    contadorMenores++;
+                                }
+                                else
+                                {
+                                    if(personas[j].edad>18 && personas[j].edad<36)
+                                    {
+                                        contadorEntre++;
+                                    }
+                                    if(personas[j].edad>35)
+                                    {
+                                        contadorMayores++;
+                                    }
+                                }
+
+                                break;
+                            }
+                            else if(personas[contador].dni==personas[j].dni)
+                            {
+                                printf("EL DNI YA FUE INGRESADO");
+                                break;
+                            }
+                        }
+
+                    }
+                    printf("Quiere seguir ingresando? s o n:  ");
+                    fflush(stdin);
+                    scanf("%c", &respuesta);
+                    if(respuesta=='s')
+                    {
+                        contador++;
+                    }
+                    }
+                        contador++;
+                        break;
+                }
+                break;
+            case 2:
+                printf("Ingrese el numero de DNI a borrar: ");
+                fflush(stdin);
+                scanf("%d", &auxDniBusqueda);
+                for(k=1;k<contador;k++)
+                {
+                    if(personas[k].dni==auxDniBusqueda)
+                    {
+                        personas[k].alta='n';
+                        flagBusqueda='s';
+                        printf("Se borro a:%s ", personas[k].nombre);
+                        break;
+                    }
+                    else if(flagBusqueda=='n')
+                    {
+                        printf("No se encontro el DNI");
+                        break;
+                    }
+                }
+                break;
+            case 3:
+                mostrarPersona(personas, contador);
+                break;
+            case 4:
+                printf("%d\t%d\t%d\n\n", contadorMenores, contadorEntre, contadorMayores);
+
+                for(men=0;men<contadorMenores;men++)
+                {
+                    columnaMenores[men]='*';
+                    printf("%c\n", columnaMenores[men]);
+
+                }
+
+                for(ent=0;ent<contadorEntre;ent++)
+                {
+                    columnaEntre[ent]='*';
+                    printf("\t%c\n", columnaEntre[ent]);
+                }
+                for(may=0;may<contadorMayores;may++)
+                {
+                    columnaMayores[may]='*';
+                    printf("\t\t%c\n", columnaMayores[may]);
+                }
+
+
+                printf("<18\t19-35\t>35\n");
+
+                break;
+            case 5:
+                seguir = 'n';
+                break;
+        }
+    }
+    return 0;
+}
+
+void mostrarPersona(eDatosPersonales personas[], int cant)
+{
+    int j;
+
+        for(j=1;j<=cant;j++)
+        {
+            printf("\n---------------");
+            printf("\n\nNOMBRE:\n\n%s ", personas[j].nombre);
+            printf("\n\nEDAD:\n\n%d ", personas[j].edad);
+            printf("\n\nDNI:\n\n%d ", personas[j].dni);
+        }
+
+
+}
+
